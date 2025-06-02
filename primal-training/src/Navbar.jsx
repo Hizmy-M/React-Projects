ffdd10812565ce86903e7cd8785d55cacc8979a9
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-02 23:56:31 +0530

    feat: created navbar

--- primal-training/src/Navbar.jsx
+++ primal-training/src/Navbar.jsx
@@ -1,3 +1,24 @@
+import AddIcon from '@mui/icons-material/Add';
+
 export default function Navbar() {
-    
+    return (
+        <>
+            <header>
+                <nav>
+                    <div className="navbar__logo">
+                        <p>Primal<span>Training</span></p>
+                    </div>
+                    <div className='navbar__list' >
+                        <AddIcon  className="navbar__menuIcon" />
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Reserve your seat</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+        </>
+    )
 }
+
