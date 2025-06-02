81f86a1e2f0169a2c9da8f1bcd85a97af42fa34b
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-02 23:58:39 +0530

    feat: navbar section -> sysntax completed

--- primal-training/src/main.jsx
+++ primal-training/src/main.jsx
@@ -1,8 +1,10 @@
 import { createRoot } from "react-dom/client";
+import Navbar from "./Navbar";
 
 const root = createRoot(document.getElementById('root'))
 root.render(
   <> 
+    <Navbar />
     <h1>Welcome</h1>
   </>
 )
