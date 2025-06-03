1278802098b8504705c051432565961a54145a14
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-03 16:36:10 +0530

    feat: BookingSection -> completed : style and semantic

--- primal-training/src/BookingSection.jsx
+++ primal-training/src/BookingSection.jsx
@@ -1,7 +1,11 @@
 export default function BookingSection() {
     return (
         <> 
-            
+            <div className="booking_container">
+                <p>What we believe in</p>
+                <h1>Join the primal tribe today!</h1>
+                <button className="primary-btn-style seconday-btn" >Reserve your spot</button>
+            </div>
         </>
     )
 }
