1c61d7c7be5b424accd5484bd7df1893637a880b
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-03 14:33:11 +0530

    feat: hero section -> completed

--- primal-training/src/HeroSection.jsx
+++ primal-training/src/HeroSection.jsx
@@ -9,13 +9,13 @@ export default function HeroSection() {
                     <img src="/src/assets/image-01.jpg" alt="hero-image" />
                 </div>
                 <div className="hero__cta">
-                   <div className="width">
+                   <div className="hero__cta_two width">
                         <h1>For the commited</h1>
                         <p>
                             Train like an athlete with top-tier equipment and expert programming.
                             Whether you're building muscle or breaking PRs, we help you push past limits.
                         </p>
-                        <button>About Us</button>
+                        <button className="primary-btn-style" >About Us</button>
                    </div>
                 </div>
             </div>
