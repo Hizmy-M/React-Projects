083b71f19214580155c362bb79467d7e79c9dd46
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-03 16:13:24 +0530

    feat: CommunitySection -> completed : desing and semantic

--- primal-training/src/CommunitySection.jsx
+++ primal-training/src/CommunitySection.jsx
@@ -1,7 +1,49 @@
 export default function CommunitySection() {
     return (
         <>
-            <h1>Community section</h1>
+            <div className="hero__container community__heading">
+                <div className="hero__heading width">
+                    <h1>Join the <span>Community</span> </h1>
+                </div>
+
+                <div className="community__container">
+                    <div className="hero__image community_image">
+                        <img src="/src/assets/image-03.jpg" alt="community-image" width="10px"/>
+                    </div>
+                    <div className="community__contents">
+                        <div className="community__contents_wrapper comunity_heading">
+                            <h1 className="width" >DISCOVER YOUR POTENTIAL</h1>
+                        </div>
+                        <div className="community__contents_wrapper ">
+                            <div className="width community_subheadings">
+                                <h1>Expert Coaching</h1>
+                                <p>Trainers who are passionate about your progrss</p>
+                            </div>
+                        </div>
+                        <div className="community__contents_wrapper ">
+                            <div className="width community_subheadings">
+                                <h1>Expert Coaching</h1>
+                                <p>Trainers who are passionate about your progrss</p>
+                            </div>
+                        </div>
+                        <div className="community__contents_wrapper ">
+                            <div className="width community_subheadings">
+                                <h1>Expert Coaching</h1>
+                                <p>Trainers who are passionate about your progrss</p>
+                            </div>
+                        </div>
+                        <div className="community__contents_wrapper">
+                            <div className="width cm_test">
+                                <button className="primary-btn-style" >About Us</button>
+                            </div>
+                        </div>
+                        
+
+                    </div>
+        
+                </div>
+                
+            </div>
         </>
     )
 }
