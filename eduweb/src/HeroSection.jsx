8b193c8cbb4092c3cee61419fe734af894eae2ed
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-04 23:33:51 +0530

    feat:  styles updated

--- eduweb/src/HeroSection.jsx
+++ eduweb/src/HeroSection.jsx
@@ -1,11 +1,11 @@
 export default function HeroSection() {
     return (
         <main>
-            <div className="hero__container">
+            <div className="hero__container wrapper">
                 <div className="hero__content">
                     <h1>Lessons and insights from 8 years</h1>
                     <p>Where to grow your business as a photographer, site or social media?</p>
-                    <button className="primary--btn">Register</button>
+                    <button className="primary-btn">Register</button>
                 </div>
                 <div className="hero__image">
                     <img src="/src/assets/image-01.png" alt="image-01" />
