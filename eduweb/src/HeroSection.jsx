e303cbc69ec19c0c82a8a7781589b494b5c02bfa
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-04 23:21:31 +0530

    Feat: Hero Section semantic completed

--- eduweb/src/HeroSection.jsx
+++ eduweb/src/HeroSection.jsx
@@ -2,7 +2,14 @@ export default function HeroSection() {
     return (
         <main>
             <div className="hero__container">
-                
+                <div className="hero__content">
+                    <h1>Lessons and insights from 8 years</h1>
+                    <p>Where to grow your business as a photographer, site or social media?</p>
+                    <button className="primary--btn">Register</button>
+                </div>
+                <div className="hero__image">
+                    <img src="/src/assets/image-01.png" alt="image-01" />
+                </div>
             </div>
         </main>
     )
