2569cc5eede55d6560de9307e1e4acc96c8cdb4f
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-06 01:09:23 +0530

    feat:  hero section completed

--- mejiwoo-web/src/HeroSection.jsx
+++ mejiwoo-web/src/HeroSection.jsx
@@ -1,11 +1,11 @@
 export default function HeroSection() {
     return (
         <main>
-            <section>
+            <section className="width">
                 <div className="hero__container">
                     <div className="hero__image-one">
-                        <img src="/src/assets/image-03.png" alt="image-01" />
-                        <img src="/src/assets/image-04.png" alt="image-01" />
+                        <img className="hero_images hide"  src="/src/assets/image-03.png" alt="image-01" />
+                        <img className="hero_images"  src="/src/assets/image-05.png" alt="image-01" />
                     </div>
                     <div className="hero__content">
                         <div className="hero__content_heading">
@@ -16,9 +16,9 @@ export default function HeroSection() {
                             <p>make your everyday look prettier with MEJIWOO Korean Made</p>
                         </div>
                     </div>
-                     <div className="hero__image-two">
-                        <img src="/src/assets/image-05.png" alt="image-01" />
-                        <img src="/src/assets/image-06.png" alt="image-01" />
+                    <div className="hero__image-two">
+                        <img className="hero_images"  src="/src/assets/image-06.png" alt="image-01" />
+                        <img className="hero_images hide"  src="/src/assets/image-04.png" alt="image-01" />
                     </div>
 
                 </div>
