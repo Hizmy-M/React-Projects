1ced41dd3cf2d08b2388dc7fc878f69c40ed6c9f
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-05 13:46:12 +0530

    feat: client section style and semantic completed

--- eduweb/src/ClientSection.jsx
+++ eduweb/src/ClientSection.jsx
@@ -1,7 +1,23 @@
 export default function ClientSection() {
     return (
         <>
-            <h1>Client section</h1>
+            <section>
+
+                <div className="client__container wrapper">
+                    <div className="client__heading">
+                        <h1>Our Clients</h1>
+                        <p>we have been working with some Fortune 500+ clients</p>
+                    </div>
+                    <div className="client__images">
+                        <img src="/src/assets/image-02.png" alt="client-01" />
+                        <img src="/src/assets/image-03.png" alt="client-02" />
+                        <img src="/src/assets/image-04.png" alt="client-03" />
+                        <img src="/src/assets/image-05.png" alt="client-04" />
+                        <img src="/src/assets/image-06.png" alt="client-05" />
+                        <img src="/src/assets/image-07.png" alt="client-06" />
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
