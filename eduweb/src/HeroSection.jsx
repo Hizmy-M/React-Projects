5ef5acbc076cce77e9afef5b158ed03c68d29ee7
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-05 16:21:11 +0530

    refactor : reposive fixed in hero section

--- eduweb/src/HeroSection.jsx
+++ eduweb/src/HeroSection.jsx
@@ -3,14 +3,14 @@ export default function HeroSection() {
         <main>
             <div className="hero__container wrapper">
                 <div className="hero__content">
-                    <h1 className="hero__contentHeading" >Lessons and insights from 8 years</h1>
+                    <h1 className="hero__contentHeading" >Lessons and insights <span className="green">from 8 years</span></h1>
                     <p>Where to grow your business as a photographer, site or social media?</p>
                     <div>
                         <button className="primary-btn">Register</button>
                     </div>
                 </div>
                 <div className="hero__image">
-                    <img src="/src/assets/image-01.png" alt="image-01" className="responsive" width="400" height="200" />
+                    <img src="/src/assets/image-01.png" alt="image-01" className="responsive" width="400" height="300" />
                 </div>
             </div>
         </main>
