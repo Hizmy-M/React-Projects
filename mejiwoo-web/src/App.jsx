0dad2bebd083e730f691a880bf1a85a0e1298637
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-06 19:56:25 +0530

    feat: customer review section done

--- mejiwoo-web/src/App.jsx
+++ mejiwoo-web/src/App.jsx
@@ -16,6 +16,7 @@ function App() {
             <BestSeller />
             <OfferSection />
             <ProductSeciont />
+            <CustomerReview />
         </>
     )
 }
--- mejiwoo-web/src/CustomerReview.jsx
+++ mejiwoo-web/src/CustomerReview.jsx
@@ -2,7 +2,56 @@ export default function CustomerReview() {
     return (
         <section>
             <div className="cusReview__container width">
-
+                <div className="cusReview__heading">
+                    <h1>What They Said</h1>
+                </div>
+                <div className="cusReview__content">
+                    <div className="cusReview__content_card">
+                        <div className="content__card_image">
+                            <img src="/src/assets/image-21.png" alt="customer-review-1" />
+                        </div>
+                        <div className="content__card_content">
+                            <div className="card__heading" >
+                                <h1>Essence Long Denim</h1>
+                                <p>-Lisa-</p>
+                            </div>
+                            <p className="card__p">
+                                “I like this pants. Its fit and perfect for me. The material is so comfortable that
+                                it can be used anywhere. It is also suitable to be combined with all types of my clothes.”
+                            </p>
+                        </div>
+                    </div>
+                    <div className="cusReview__content_card">
+                        <div className="content__card_image">
+                            <img src="/src/assets/image-22.png" alt="customer-review-2" />
+                        </div>
+                        <div className="content__card_content">
+                            <div className="card__heading">
+                                <h1>Essence Long Denim</h1>
+                                <p>-Lisa-</p>
+                            </div>
+                            <p className="card__p">
+                                “I like this pants. Its fit and perfect for me. The material is so comfortable that
+                                it can be used anywhere. It is also suitable to be combined with all types of my clothes.”
+                            </p>
+                        </div>
+                    </div>
+                    <div className="cusReview__content_card">
+                        <div className="content__card_image">
+                            <img src="/src/assets/image-23.png" alt="customer-review-3" />
+                        </div>
+                        <div className="content__card_content">
+                            <div className="card__heading">
+                                <h1>Essence Long Denim</h1>
+                                <p>-Lisa-</p>
+                            </div>
+                            <p className="card__p">
+                                “I like this pants. Its fit and perfect for me. The material is so comfortable that
+                                it can be used anywhere. It is also suitable to be combined with all types of my clothes.”
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
