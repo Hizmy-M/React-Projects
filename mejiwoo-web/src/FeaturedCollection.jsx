7442db751264cae43037fbfb6d0ba896b28722ef
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-06 19:19:31 +0530

    info : not required now

---
+++ mejiwoo-web/src/FeaturedCollection.jsx
@@ -0,0 +1,43 @@
+export default function FeaturedCollection() {
+    return (
+        <section>
+            <div className="featured__container width ">
+                <div className="featured__heading">
+                    <h1>Our Featured Collections</h1>
+                </div>
+                <div className="featured__contents">
+                    <div className="featured__card">
+                        <img src="/src/assets/image-20.png" alt="one" />
+                        <div className="bottom-left">
+                            Colorfull knitwear series
+                        </div>
+                    </div>
+                    <div className="featured__card">
+                        <img src="/src/assets/image-20.png" alt="one" />
+                        <div className="bottom-left">
+                            Colorfull knitwear series
+                        </div>
+                    </div>
+                    <div className="featured__card">
+                        <img src="/src/assets/image-20.png" alt="one" />
+                        <div className="bottom-left">
+                            Colorfull knitwear series
+                        </div>
+                    </div>
+                    <div className="featured__card">
+                        <img src="/src/assets/image-20.png" alt="one" />
+                        <div className="bottom-left">
+                            Colorfull knitwear series
+                        </div>
+                    </div>
+                    <div className="featured__card">
+                        <img src="/src/assets/image-20.png" alt="one" />
+                        <div className="bottom-left">
+                            Colorfull knitwear series
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
