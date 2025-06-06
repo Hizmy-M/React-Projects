72c4243f9e71a4596ee39b15660e1742be41c582
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-06 21:57:14 +0530

    feat : footer completed

--- mejiwoo-web/src/Footer.jsx
+++ mejiwoo-web/src/Footer.jsx
@@ -1,5 +1,35 @@
 export default function Footer() {
     return (
-        
+    <footer>
+        <div className="footer__container">
+            <div className="footer__container_one width">
+                <div className="footer__heading">
+                    <h1>Mejiwoo</h1>
+                    <p>미지우</p>
+                </div>
+                <div className="footer__one">
+                    <h2>About Us</h2>
+                    <ul>
+                        <li>News</li>
+                        <li>Official Store</li>
+                        <li>Compnay</li>
+                        <li>Careers</li>
+                    </ul>
+                </div>
+                <div className="footer__one">
+                    <h2>Get Help</h2>
+                    <ul>
+                        <li>FAQ</li>
+                        <li>Shipping</li>
+                        <li>Payment</li>
+                        <li>Return</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="footer__container_two">
+                <span>@2023 MEJIWOO. All Rights Reserved</span>
+            </div>
+        </div>
+    </footer>
     )
 }
