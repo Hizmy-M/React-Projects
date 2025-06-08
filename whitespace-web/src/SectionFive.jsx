14111cbc8428eb0f0da73d57cf00977cde26bc8c
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-08 15:27:13 +0530

    feat: file created -> semantic done

---
+++ whitespace-web/src/SectionFive.jsx
@@ -0,0 +1,22 @@
+export default function SectionFive() {
+    return (
+        <main>
+            <div className="section__container">
+                <div className="section__one">
+                    <div className="section__one-heading">
+                        <h1>100% your data</h1>
+                        <p>
+                            The app is open source and your notes are saved to open format, so you'll always have access to them.
+                            Uses End-To-Encryption (E2EE) to secure your notes and ensure no-one but yourself can 
+                            access them.   
+                        </p>
+                        <button>Read more</button>
+                    </div>
+                </div>
+                <div className="section__two">
+                    <img src="/src/assets/image-03.svg" alt="section-two" />
+                </div>
+            </div>
+        </main>
+    )
+}
