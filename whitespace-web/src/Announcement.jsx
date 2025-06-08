d38d18b047f9a3d897a1520d091efc3d2f8c52e7
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-08 15:22:50 +0530

    feat: file created -> semantic done

---
+++ whitespace-web/src/Announcement.jsx
@@ -0,0 +1,18 @@
+export default function Announcement() {
+    return (
+        <section>
+            <div className="plan__container">
+                <div className="plan__one">
+                     <div className="plan__one-heading">
+                        <h1>Customise it to your needs</h1>
+                        <p>
+                            Customise the app with plugins, customise themes and multipletext editors (Rich Text
+                            or Markdown) or create your own scripts and plugins using the Extension API. 
+                        </p>
+                        <button>Let's Go</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
