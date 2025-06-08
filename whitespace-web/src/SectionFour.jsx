0ecca4735a16049667c2be8df7fc2be8a955a9f9
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-08 13:45:30 +0530

    feat: file created -> semantic done

---
+++ whitespace-web/src/SectionFour.jsx
@@ -0,0 +1,21 @@
+export default function SectionFour() {
+    return (
+        <main>
+            <div className="section__container">
+                <div className="section__one">
+                    <div className="section__one-heading">
+                        <h1>Customise it to your needs</h1>
+                        <p>
+                            Customise the app with plugins, customise themes and multipletext editors (Rich Text
+                            or Markdown) or create your own scripts and plugins using the Extension API. 
+                        </p>
+                        <button>Let's Go</button>
+                    </div>
+                </div>
+                <div className="section__two">
+                    <img src="/src/assets/image-03.svg" alt="section-two" />
+                </div>
+            </div>
+        </main>
+    )
+}
---
+++ whitespace-web/src/SectionThree.jsx
@@ -0,0 +1,21 @@
+export default function SectionThree() {
+    return (
+        <main>
+            <div className="section__container">
+                <div className="section__one">
+                    <div className="section__one-heading">
+                        <h1>Use as Extension</h1>
+                        <p>
+                            Use the web clipper extension, available on Chrome and Firefox, to save web
+                            pages or take screenshots as notes.
+                        </p>
+                        <button>Let's Go</button>
+                    </div>
+                </div>
+                <div className="section__two">
+                    <img src="/src/assets/image-03.svg" alt="section-two" />
+                </div>
+            </div>
+        </main>
+    )
+}
