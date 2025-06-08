d2a0c4c84e9d40e61bc832c0913931fe814e10e1
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-08 13:26:57 +0530

    feat: hero section semantic done

--- whitespace-web/src/HeroSection.jsx
+++ whitespace-web/src/HeroSection.jsx
@@ -4,7 +4,12 @@ export default function HeroSection() {
             <div className="section__container">
                 <div className="section__one">
                     <div className="section__one-heading">
-                        
+                        <h1>Get More Done with whitespace</h1>
+                        <p>
+                            Project management software that enables your teams toc collaborate, plan analyze
+                            and manage everyday task
+                        </p>
+                        <button>Try Whitespace </button>
                     </div>
                 </div>
                 <div className="section__two">
