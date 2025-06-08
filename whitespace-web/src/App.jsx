d515e2a2f87982b46baca48f55119217273d06c7
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-08 20:12:06 +0530

    feat: added compoenets

--- whitespace-web/src/App.jsx
+++ whitespace-web/src/App.jsx
@@ -1,9 +1,34 @@
 import Header from "./Header"
+import HeroSection from "./HeroSection"
+import SectionOne from "./SectionOne"
+import SectionTwo from "./SectionTwo"
+import SectionThree from "./SectionThree"
+import SectionFour from "./SectionFour"
+import SectionFive from "./SectionFive"
+import SectionSix from "./SectionSix"
+import Footer from "./Footer"
+import ReviewSection from "./ReviewSection"
+import ChoosePlanSection from "./ReviewSection"
+import SponsorSection from "./SponsorSection"
+import Announcement from "./Announcement"
+
 
 export default function App() {
     return (
         <>
             <Header />
+            <HeroSection />
+            <SectionOne />
+            <SectionTwo />
+            <SectionThree />
+            <SectionFour />
+            <ChoosePlanSection />
+            <Announcement />
+            <SectionFive />
+            <SponsorSection />
+            <SectionSix />
+            <ReviewSection />
+            <Footer />
         </>
     )
 }
