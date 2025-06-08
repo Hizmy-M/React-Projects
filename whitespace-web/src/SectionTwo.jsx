6eda73aae05acc8b300e070f34411032861f401d
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-08 13:30:59 +0530

    feat: section two semantic done

---
+++ whitespace-web/src/SectionTwo.jsx
@@ -0,0 +1,21 @@
+export default function SectionTwo() {
+    return (
+        <main>
+            <div className="section__container">
+                <div className="section__one">
+                    <div className="section__one-heading">
+                        <h1>Work together</h1>
+                        <p>
+                            With whitespace, share your notes with your colleges and collaborate on them.
+                            You can also publish a note to the internet and share the URL with others. 
+                        </p>
+                        <button>Try it now</button>
+                    </div>
+                </div>
+                <div className="section__two">
+                    <img src="/src/assets/image-03.svg" alt="section-two" />
+                </div>
+            </div>
+        </main>
+    )
+}
