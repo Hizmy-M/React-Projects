f7371423c91a5e268b3c7f34ff9c989fdb222e8a
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-08 15:41:47 +0530

    feat: file created -> section one done

---
+++ whitespace-web/src/SponsorSection.jsx
@@ -0,0 +1,17 @@
+export default function SponsorSection() {
+    return (
+        <section>
+            <div className="plan__container">
+                <div className="plan__one">
+                     <div className="plan__one-heading">
+                        <h1>Our Sponsors</h1>
+                    </div>
+                </div>
+                <div className="plan__images">
+                    {/* todo */}
+                    {/* add logos */}
+                </div>
+            </div>
+        </section>
+    )
+}
