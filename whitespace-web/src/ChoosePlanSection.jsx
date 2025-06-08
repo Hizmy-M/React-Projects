2bcff5dcdd27de3ccfe8489018ca31cc9c19afba
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-08 15:21:22 +0530

    feat: semantic done

---
+++ whitespace-web/src/ChoosePlanSection.jsx
@@ -0,0 +1,74 @@
+export default function ChoosePlanSection() {
+    return (
+        <section>
+            <div className="plan__container">
+                <div className="plan__one">
+                     <div className="plan__one-heading">
+                        <h1>Customise it to your needs</h1>
+                        <p>
+                            Customise the app with plugins, customise themes and multipletext editors (Rich Text
+                            or Markdown) or create your own scripts and plugins using the Extension API. 
+                        </p>
+                        <button>Let's Go</button>
+                    </div>
+                </div>
+                <div className="plan__two">
+                    <div className="card">
+                        <div className="card__heading">
+                            <p>Personal</p>
+                            <p>$11.99</p>
+                        </div>
+                        <div className="card__contents">
+                            <p>Capture Ideas and find them quickly</p>
+                            <ul>
+                                <li>Sync United Data</li>
+                                <li>10GB Monthly uploads</li>
+                                <li>200 MB max note size</li>
+                                <li>Customize Home dashboard and access extra widgets</li>
+                                <li>Connecti primary Google Calender account</li>
+                                <li>Add due dates, reminders and notifications to your tasks</li>
+                            </ul>
+                            <button>Get Started</button>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card__heading">
+                            <p>Organizations</p>
+                            <p>$49.99</p>
+                        </div>
+                        <div className="card__contents">
+                            <p>Capture Ideas and find them quickly</p>
+                            <ul>
+                                <li>Sync United Data</li>
+                                <li>10GB Monthly uploads</li>
+                                <li>200 MB max note size</li>
+                                <li>Customize Home dashboard and access extra widgets</li>
+                                <li>Connecti primary Google Calender account</li>
+                                <li>Add due dates, reminders and notifications to your tasks</li>
+                            </ul>
+                            <button>Get Started</button>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card__heading">
+                            <p>Free</p>
+                            <p>$0</p>
+                        </div>
+                        <div className="card__contents">
+                            <p>Capture Ideas and find them quickly</p>
+                            <ul>
+                                <li>Sync United Data</li>
+                                <li>10GB Monthly uploads</li>
+                                <li>200 MB max note size</li>
+                                <li>Customize Home dashboard and access extra widgets</li>
+                                <li>Connecti primary Google Calender account</li>
+                                <li>Add due dates, reminders and notifications to your tasks</li>
+                            </ul>
+                            <button>Get Started</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
