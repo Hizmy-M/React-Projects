a2e03c61160bdd0d25767918656b6940896a5676
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-08 13:26:17 +0530

    feat:  file created section -> semantic done

---
+++ whitespace-web/src/SectionOne.jsx
@@ -0,0 +1,21 @@
+export default function SectionOne() {
+    return (
+        <main>
+            <div className="section__container">
+                <div className="section__one">
+                    <div className="section__one-heading">
+                        <h1>Project Management</h1>
+                        <p>
+                            Images, videos, PDFs and audio files are supported. Create math expressions and
+                            diagrams directly from the app. Take photos with the mobile app and save them to a note  
+                        </p>
+                        <button>Get Started </button>
+                    </div>
+                </div>
+                <div className="section__two">
+                    <img src="/src/assets/image-02.svg" alt="section-one" />
+                </div>
+            </div>
+        </main>
+    )
+}
