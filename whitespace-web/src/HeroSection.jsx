cefb36dfc63585dd8dbd83e74ac6f48defa9b104
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-08 13:16:38 +0530

    feat: file created

---
+++ whitespace-web/src/HeroSection.jsx
@@ -0,0 +1,16 @@
+export default function HeroSection() {
+    return (
+        <main>
+            <div className="section__container">
+                <div className="section__one">
+                    <div className="section__one-heading">
+                        
+                    </div>
+                </div>
+                <div className="section__two">
+                    <img src="/src/assets/image-01.svg" alt="hero-iamge" />
+                </div>
+            </div>
+        </main>
+    )
+}
