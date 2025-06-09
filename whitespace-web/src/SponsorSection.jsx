883f3580d2556e720f4a12272f6be48c05cb236f
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-09 16:58:38 +0530

    feat: sponsor section completed

--- whitespace-web/src/SponsorSection.jsx
+++ whitespace-web/src/SponsorSection.jsx
@@ -1,15 +1,20 @@
 export default function SponsorSection() {
     return (
         <section>
-            <div className="plan__container">
-                <div className="plan__one">
+            <div className="plan__container sponsor__container">
+                <div className="plan__one sponsor__one ">
                      <div className="plan__one-heading">
                         <h1>Our Sponsors</h1>
                     </div>
                 </div>
-                <div className="plan__images">
+                <div className="sponsor__images">
                     {/* todo */}
                     {/* add logos */}
+                    <img src="/src/assets/sponsor-one.png" alt="sponsor-one" />
+                    <img src="/src/assets/sponsor-two.png" alt="sponsor-one" />
+                    <img src="/src/assets/sponsor-three.png" alt="sponsor-one" />
+                    <img src="/src/assets/sponsor-four.png" alt="sponsor-one" />
+                    <img src="/src/assets/sponsor-five.png" alt="sponsor-one" />
                 </div>
             </div>
         </section>
