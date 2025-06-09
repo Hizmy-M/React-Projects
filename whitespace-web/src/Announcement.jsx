95a2da525885adba7bb27a7f696790ea1d2c2966
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-09 16:45:40 +0530

    feat: announcement section -> styling completed

--- whitespace-web/src/Announcement.jsx
+++ whitespace-web/src/Announcement.jsx
@@ -1,16 +1,19 @@
+import KeyboardDoubleArrowRightTwoToneIcon from '@mui/icons-material/KeyboardDoubleArrowRightTwoTone';
+
+
 export default function Announcement() {
     return (
         <section>
             <div className="plan__container">
-                <div className="plan__one">
-                     <div className="plan__one-heading">
+                <div className="plan__one announcement__container ">
+                     <div className="plan__one-heading annoucement__contents">
                         <h1>Your work everywhere you are</h1>
                         <p>
                             Access your notes from your computer, phone or tablet by synchronising with various
                             services, including whitespace, Dropbox and OneDrive. The app is available on Windows,
                             macOs, Linux, Android and IOS. A terminal app is also available!    
                         </p>
-                        <button>Try Taskey</button>
+                        <button className="primary-btn" >Try Whitespace <KeyboardDoubleArrowRightTwoToneIcon /></button>
                     </div>
                 </div>
             </div>
