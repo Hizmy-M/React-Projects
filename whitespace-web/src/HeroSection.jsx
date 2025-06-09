eb9bbfda70dab4d1b58cae011d93599e4a4d5ad3
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-09 14:06:18 +0530

    feat: main section mobile view done

--- whitespace-web/src/HeroSection.jsx
+++ whitespace-web/src/HeroSection.jsx
@@ -1,7 +1,10 @@
+import KeyboardDoubleArrowRightTwoToneIcon from '@mui/icons-material/KeyboardDoubleArrowRightTwoTone';
+
+
 export default function HeroSection() {
     return (
-        <main>
-            <div className="section__container">
+        <main className="main__container">
+            <div className="section__container width">
                 <div className="section__one">
                     <div className="section__one-heading">
                         <h1>Get More Done with whitespace</h1>
@@ -9,11 +12,11 @@ export default function HeroSection() {
                             Project management software that enables your teams toc collaborate, plan analyze
                             and manage everyday task
                         </p>
-                        <button>Try Whitespace </button>
+                        <button className="primary-btn" >Try Whitespace <KeyboardDoubleArrowRightTwoToneIcon /></button>
                     </div>
                 </div>
                 <div className="section__two">
-                    <img src="/src/assets/image-01.svg" alt="hero-iamge" />
+                    <img src="/src/assets/image-06.svg" alt="hero-iamge" />
                 </div>
             </div>
         </main>
