c0bcfc7fbc2ecd998df4ca06f856ae3228105c5d
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-10 23:13:33 +0530

    feat: section three semantic done

---
+++ 05.terraConnect-web/src/SectionThree.jsx
@@ -0,0 +1,46 @@
+export default function SectionThree() {
+    return (
+        <section>
+            <div className="section__container">
+                <div className="section__one">
+                    <div className="section__one-heading">
+                        <span>Contact Us</span>
+                        <h2>Got any questions? Don't hesitate to get in touch.</h2>
+                    </div>
+                    <div className="section__one-content">
+                        <div className="section__one-card">
+                            <div className="section__one-card-image">
+                                <img src="/src/assets/icon-12.png" alt="icon-12" />
+                            </div>
+                            <div className="section__one-card-content">
+                                <p>Address</p>
+                                <span>Moonshine St. 14/05 Light City, London</span>
+                            </div>
+                        </div>
+                        <div className="section__one-card">
+                            <div className="section__one-card-image">
+                                <img src="/src/assets/icon-13.png" alt="icon-13" />
+                            </div>
+                            <div className="section__one-card-content">
+                                <p>Phone</p>
+                                <span>00(123)4567890</span>
+                            </div>
+                        </div>
+                        <div className="section__one-card">
+                            <div className="section__one-card-image">
+                                <img src="/src/assets/icon-14.png" alt="icon-14" />
+                            </div>
+                            <div className="section__one-card-content">
+                                <p>E-Mail</p>
+                                <span>sandbox@email.com</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="section__two">
+                    <img src="/src/assets/image-10.png" alt="" />
+                </div>
+            </div>
+        </section>
+    )
+}
