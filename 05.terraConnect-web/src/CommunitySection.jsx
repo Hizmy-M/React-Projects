3a9c64c99b4259a70832d89fe34a8b9f23552d49
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-10 23:19:30 +0530

    feat: File created -> community section : semantic completed

---
+++ 05.terraConnect-web/src/CommunitySection.jsx
@@ -0,0 +1,11 @@
+export default function CommunitySection() {
+    return (
+        <section>
+            <div className="community__section">
+                <span>Join Our Community</span>
+                <h1>We are trusted by over 5000+ clients. Join them now and grow your business.</h1>
+                <button className="primary--button">Get Started</button>
+            </div>
+        </section>
+    )
+}
