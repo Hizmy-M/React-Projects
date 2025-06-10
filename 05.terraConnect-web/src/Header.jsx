a6059c804787a54fcafa862a2b1651d943e05497
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-10 17:49:18 +0530

    Header semantic done

--- 05.terraConnect-web/src/Header.jsx
+++ 05.terraConnect-web/src/Header.jsx
@@ -1,5 +1,25 @@
 export default function Header() {
     return (
-        <h1>Header</h1>
+        <header>
+            <div className="header__navigation">
+                <div className="header__logo">
+                    <img src="/src/assets/company-logo.png" alt="terraconnect-logo" />
+                    <h1>Terra<span className="logo-color">Connect</span></h1>
+                </div>
+                <nav className="header__nav">
+                    <ul>
+                        <li>Demos</li>
+                        <li>Products</li>
+                        <li>Pages</li>
+                        <li>Projects</li>
+                        <li>Blog</li>
+                        <li>Documentation</li>
+                    </ul>
+                    <div className="header__cta">
+                        <button className="primary-button" >Contact Us</button>
+                    </div>
+                </nav>
+            </div>
+        </header>
     )
 }
