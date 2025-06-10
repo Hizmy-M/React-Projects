0cf27d417c3abab3a5bf86848b0ead89794ee8fa
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-10 18:54:04 +0530

    feat: feature section -> semantic done

---
+++ 05.terraConnect-web/src/FeatureSection.jsx
@@ -0,0 +1,74 @@
+export default function FeatureSection() {
+    return (
+        <section>
+            <div className="feature__container">
+                <div className="feature__one">
+                    <h3>Our Features</h3>
+                    <p>The service we offer is specifically designed to meet your needs.</p>
+                </div>
+                <div className="feature__two">
+                    <div className="feature__card">
+                        <img src="/src/assets/icon-01.png" alt="icon-one" />
+                        <div className="card__content">
+                            <h2>Software & Interegation</h2>
+                            <p>
+                                Duis mollis gravida commodo id luctus era porttitor ligula, 
+                                eget lacinia odio sem aget elit nullam quis risus eget.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="feature__card">
+                        <img src="/src/assets/icon-02.png" alt="icon-one" />
+                        <div className="card__content">
+                            <h2>Network % Security</h2>
+                            <p>
+                                Duis mollis gravida commodo id luctus era porttitor ligula, 
+                                eget lacinia odio sem aget elit nullam quis risus eget.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="feature__card">
+                        <img src="/src/assets/icon-03.png" alt="icon-one" />
+                        <div className="card__content">
+                            <h2>Cloud Services</h2>
+                            <p>
+                                Duis mollis gravida commodo id luctus era porttitor ligula, 
+                                eget lacinia odio sem aget elit nullam quis risus eget.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="feature__card">
+                        <img src="/src/assets/icon-04.png" alt="icon-one" />
+                        <div className="card__content">
+                            <h2>Wireless Networking</h2>
+                            <p>
+                                Duis mollis gravida commodo id luctus era porttitor ligula, 
+                                eget lacinia odio sem aget elit nullam quis risus eget.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="feature__card">
+                        <img src="/src/assets/icon-05.png" alt="icon-one" />
+                        <div className="card__content">
+                            <h2>IT Solutions</h2>
+                            <p>
+                                Duis mollis gravida commodo id luctus era porttitor ligula, 
+                                eget lacinia odio sem aget elit nullam quis risus eget.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="feature__card">
+                        <img src="/src/assets/icon-06.png" alt="icon-one" />
+                        <div className="card__content">
+                            <h2>Server Configuration</h2>
+                            <p>
+                                Duis mollis gravida commodo id luctus era porttitor ligula, 
+                                eget lacinia odio sem aget elit nullam quis risus eget.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
