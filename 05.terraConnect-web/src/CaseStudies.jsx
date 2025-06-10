ff2be5486cc91533193c1f8f8a2757d1cc4c7957
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-10 20:44:18 +0530

    feat: case study section -> semantic done

---
+++ 05.terraConnect-web/src/CaseStudies.jsx
@@ -0,0 +1,26 @@
+export default function CaseStudies() {
+    return (
+        <section>
+            <div className="case-studies__container">
+                <div className="case-studies__one">
+                    <span>Case Studies</span>
+                    <h2>Check out some of our awesome projects with creative ideas and great design.</h2>
+                </div>
+                <div className="case-studies__two">
+                    <div className="case-studies__two-card">
+                        <img src="/src/assets/image-04.png" alt="image-04" />
+                        <p>Ligula tristique quis risus</p>
+                    </div>
+                    <div className="case-studies__two-card">
+                        <img src="/src/assets/image-05.png" alt="image-04" />
+                        <p>Nullam id dolor elit id nibh</p>
+                    </div>
+                    <div className="case-studies__two-card">
+                        <img src="/src/assets/image-06.png" alt="image-04" />
+                        <p>Ultricies fusce porta elit</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
