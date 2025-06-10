609a094c048b57268a14b1a2420f1a5304e0498b
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-11 02:27:11 +0530

    feature section style done

--- 05.terraConnect-web/src/FeatureSection.jsx
+++ 05.terraConnect-web/src/FeatureSection.jsx
@@ -1,24 +1,29 @@
 export default function FeatureSection() {
     return (
-        <section>
-            <div className="feature__container">
+        <section className="extra-space">
+            <div className="feature__container width">
                 <div className="feature__one">
-                    <h3>Our Features</h3>
-                    <p>The service we offer is specifically designed to meet your needs.</p>
+                    <span className="logo-color highlite-title">Our Features</span>
+                    <h2>The service we offer is specifically <br /> designed to meet your needs.</h2>
                 </div>
                 <div className="feature__two">
-                    <div className="feature__card">
-                        <img src="/src/assets/icon-01.png" alt="icon-one" />
-                        <div className="card__content">
-                            <h2>Software & Interegation</h2>
-                            <p>
-                                Duis mollis gravida commodo id luctus era porttitor ligula, 
-                                eget lacinia odio sem aget elit nullam quis risus eget.
-                            </p>
+                    <div className="feature__two-cards">
+                        <div className="feature__card">
+                            <div className="card__image">
+                                <img className="icon-responsive" src="/src/assets/icon-01.png" alt="icon-one" />
+                            </div>
+                            <div className="card__content">
+                                <h2>Software & Interegation</h2>
+                                <p>
+                                    Duis mollis gravida commodo id luctus era porttitor ligula, 
+                                    eget lacinia odio sem aget elit nullam quis risus eget.
+                                </p>
+                            </div>
                         </div>
-                    </div>
                     <div className="feature__card">
-                        <img src="/src/assets/icon-02.png" alt="icon-one" />
+                        <div className="card__image">
+                            <img  className="icon-responsive" src="/src/assets/icon-02.png" alt="icon-one" />
+                        </div>
                         <div className="card__content">
                             <h2>Network % Security</h2>
                             <p>
@@ -28,7 +33,9 @@ export default function FeatureSection() {
                         </div>
                     </div>
                     <div className="feature__card">
-                        <img src="/src/assets/icon-03.png" alt="icon-one" />
+                        <div className="card__image">
+                            <img  className="icon-responsive" src="/src/assets/icon-03.png" alt="icon-one" />
+                        </div>                        
                         <div className="card__content">
                             <h2>Cloud Services</h2>
                             <p>
@@ -37,34 +44,43 @@ export default function FeatureSection() {
                             </p>
                         </div>
                     </div>
-                    <div className="feature__card">
-                        <img src="/src/assets/icon-04.png" alt="icon-one" />
-                        <div className="card__content">
-                            <h2>Wireless Networking</h2>
-                            <p>
-                                Duis mollis gravida commodo id luctus era porttitor ligula, 
-                                eget lacinia odio sem aget elit nullam quis risus eget.
-                            </p>
-                        </div>
                     </div>
-                    <div className="feature__card">
-                        <img src="/src/assets/icon-05.png" alt="icon-one" />
-                        <div className="card__content">
-                            <h2>IT Solutions</h2>
-                            <p>
-                                Duis mollis gravida commodo id luctus era porttitor ligula, 
-                                eget lacinia odio sem aget elit nullam quis risus eget.
-                            </p>
+                    <div className="feature__two-cards">
+                        <div className="feature__card">
+                            <div className="card__image">
+                                <img  className="icon-responsive" src="/src/assets/icon-04.png" alt="icon-one" />
+                            </div>                          
+                            <div className="card__content">
+                                <h2>Wireless Networking</h2>
+                                <p>
+                                    Duis mollis gravida commodo id luctus era porttitor ligula, 
+                                    eget lacinia odio sem aget elit nullam quis risus eget.
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="feature__card">
-                        <img src="/src/assets/icon-06.png" alt="icon-one" />
-                        <div className="card__content">
-                            <h2>Server Configuration</h2>
-                            <p>
-                                Duis mollis gravida commodo id luctus era porttitor ligula, 
-                                eget lacinia odio sem aget elit nullam quis risus eget.
-                            </p>
+                        <div className="feature__card">
+                            <div className="card__image">
+                                <img  className="icon-responsive" src="/src/assets/icon-05.png" alt="icon-one" />
+                            </div>                                 
+                            <div className="card__content">
+                                <h2>IT Solutions</h2>
+                                <p>
+                                    Duis mollis gravida commodo id luctus era porttitor ligula, 
+                                    eget lacinia odio sem aget elit nullam quis risus eget.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="feature__card">
+                            <div className="card__image">
+                                <img  className="icon-responsive" src="/src/assets/icon-06.png" alt="icon-one" />
+                            </div>                         
+                            <div className="card__content">
+                                <h2>Server Configuration</h2>
+                                <p>
+                                    Duis mollis gravida commodo id luctus era porttitor ligula, 
+                                    eget lacinia odio sem aget elit nullam quis risus eget.
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
