1715131341f8adb4e9c421ec15a29367229c936d
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-10 20:27:24 +0530

    feat: about-us section -> semantic done

---
+++ 05.terraConnect-web/src/AboutUs.jsx
@@ -0,0 +1,77 @@
+export default function AboutUs() {
+    return (
+        <section>
+            <div className="about-us__container">
+                <div className="about-us__one">
+                    <div className="about-us__one-heading">
+                        <h1>We are proud of our works</h1>
+                        <p>We bring solutions to make life easier for our customers</p>
+                    </div>
+                    <div className="about-us__one-content">
+                        <div className="one-content__card">
+                            <img src="/src/assets/icon-08.png" alt="icon-08" />
+                            <span>1000+</span>
+                            <p>Completed Projects</p>
+                        </div>
+                        <div className="one-content__card">
+                            <img src="/src/assets/icon-09.png" alt="icon-08" />
+                            <span>4x</span>
+                            <p>Revenue Growth</p>
+                        </div>
+                        <div className="one-content__card">
+                            <img src="/src/assets/icon-08.png" alt="icon-08" />
+                            <span>99.7%</span>
+                            <p>Customer Satisfaction</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="about-us__two">
+                    <div className="about-us__two-card">
+                        <p>
+                            “Vivamus sagittis lacus vel
+                            augue laoreet rutrum
+                            faucibus dolor auctor.
+                            Vestibulum id ligula porta.
+                            Cras mattis consectetur.”
+                        </p>
+                        <h2>Coriss Ambady</h2>
+                        <p>Financial Analyst</p>
+                    </div>
+                    <div className="about-us__two-card">
+                        <p>
+                            “Fusce dapibus, tellus ac
+                            cursus tortor mauris
+                            condimentum fermentum
+                            massa justo sit amet purus
+                            sit amet fermentum.”
+                        </p>
+                        <h2>Cory Zamora</h2>
+                        <p>Marketing Specialist</p>
+                    </div>
+                    <div className="about-us__two-card">
+                        <p>
+                            “Curabitur blandit tempus
+                            porttitor. Vivamus sagittis
+                            lacus vel augue laoreet
+                            rutrum faucibus dolor eu
+                            rutrum. Nulla vitae libero.”
+                        </p>
+                        <h2>Nikolas Brooten</h2>
+                        <p>Sales Manager</p>
+                    </div>
+                    <div className="about-us__two-card">
+                        <p>
+                            “Etiam adipiscing tincidunt
+                            elit convallis felis suscipit ut.
+                            Phasellus rhoncus eu
+                            tincidunt auctor nullam
+                            rutrum, pharetra augue.”
+                        </p>
+                        <h2>Coriss Ambady</h2>
+                        <p>Financial Analyst</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
