2abcb9c2bd742b007591a3d2dcb62cf429b76b12
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-10 23:06:48 +0530

    feat: about us semantic -> done

---
+++ 05.terraConnect-web/src/AboutUs.jsx
@@ -0,0 +1,83 @@
+export default function AboutUs() {
+    return (
+        <section>
+            <div className="about-us__container">
+                <div className="about-us__one">
+                    <div className="about-us__one-images">
+                        <div className="two-image">
+                            <img src="/src/assets/image-07.png" alt="image-07" />
+                            <img src="/src/assets/image-08.png" alt="image-08" />
+                        </div>
+                        <div className="one-image">
+                            <img src="/src/assets/image-09.png" alt="image-09" />
+                        </div>
+                    </div>
+                    <div className="about-us__one-content">
+                        <div className="one-content-heading">
+                            <p>Who Are We?</p>
+                            <h2>Company that believes in the power of creative strategy.</h2>
+                            <p> 
+                                Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
+                                ridiculus mus. Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+                                Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Cum sociis
+                                natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                                Aenean lacinia bibendum nulla sed.
+                            </p>
+                        </div>
+                        <div className="one-content-notes">
+                            <ul>
+                                <li>Aenean eu leo quam ornare curabitur blandit tempus.</li>
+                                <li>Nullam quis risus eget urna mollis ornare donec elit.</li>
+                            </ul>
+                            <ul>
+                                <li>Aenean eu leo quam ornare curabitur blandit tempus.</li>
+                                <li>Nullam quis risus eget urna mollis ornare donec elit.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="about-us__two">
+                    <div className="about-us__card">
+                        <div className="card__heading">
+                            <span>1</span>
+                        </div>
+                        <div className="card__content">
+                            <h2>Our Vision</h2>
+                            <p>
+                                Nulla vitae elit libero, a pharetra augue.
+                                Donec id elit non mi porta gravida at eget.
+                                Fusce dapibus tellus.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="about-us__card">
+                        <div className="card__heading">
+                            <span>1</span>
+                        </div>
+                        <div className="card__content">
+                            <h2>Our Vision</h2>
+                            <p>
+                                Nulla vitae elit libero, a pharetra augue.
+                                Donec id elit non mi porta gravida at eget.
+                                Fusce dapibus tellus.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="about-us__card">
+                        <div className="card__heading">
+                                <span>1</span>
+                        </div>
+                        <div className="card__content">
+                            <h2>Our Vision</h2>
+                            <p>
+                                Nulla vitae elit libero, a pharetra augue.
+                                Donec id elit non mi porta gravida at eget.
+                                Fusce dapibus tellus.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
