f6db65cbbf3b94d7ed706f382ef8ab0fcdcab2cd
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-11 03:41:36 +0530

    added extra space

--- 05.terraConnect-web/src/ReviewSection.jsx
+++ 05.terraConnect-web/src/ReviewSection.jsx
@@ -1,75 +1,80 @@
 export default function ReviewSection() {
     return (
-        <section>
-            <div className="review__container">
+        <section className="backgorund extra-space">
+            <div className="review__container width">
                 <div className="review__one">
                     <div className="review__one-heading">
-                        <h1>We are proud of our works</h1>
-                        <p>We bring solutions to make life easier for our customers</p>
+                        <h2>We are proud <br /> of our works</h2>
+                        <p>We bring solutions to make <br />  life easier for our customers</p>
                     </div>
                     <div className="review__one-content">
                         <div className="one-content__card">
-                            <img src="/src/assets/icon-08.png" alt="icon-08" />
+                            <img className="icon-responsive" src="/src/assets/icon-09.png" alt="icon-08" />
                             <span>1000+</span>
                             <p>Completed Projects</p>
                         </div>
                         <div className="one-content__card">
-                            <img src="/src/assets/icon-09.png" alt="icon-08" />
+                            <img className="icon-responsive"  src="/src/assets/icon-10.png" alt="icon-08" />
                             <span>4x</span>
                             <p>Revenue Growth</p>
                         </div>
                         <div className="one-content__card">
-                            <img src="/src/assets/icon-08.png" alt="icon-08" />
+                            <img className="icon-responsive"  src="/src/assets/icon-11.png" alt="icon-08" />
                             <span>99.7%</span>
                             <p>Customer Satisfaction</p>
                         </div>
                     </div>
                 </div>
                 <div className="review__two">
-                    <div className="review__two-card">
-                        <p>
-                            “Vivamus sagittis lacus vel
-                            augue laoreet rutrum
-                            faucibus dolor auctor.
-                            Vestibulum id ligula porta.
-                            Cras mattis consectetur.”
-                        </p>
-                        <h2>Coriss Ambady</h2>
-                        <p>Financial Analyst</p>
+                    <div className="review__two-d">
+                        <div className="review__two-card">
+                            <p>
+                                “Vivamus sagittis lacus vel
+                                augue laoreet rutrum
+                                faucibus dolor auctor.
+                                Vestibulum id ligula porta.
+                                Cras mattis consectetur.”
+                            </p>
+                            <h2>Coriss Ambady</h2>
+                            <p>Financial Analyst</p>
+                        </div>
+                        <div className="review__two-card">
+                            <p>
+                                “Fusce dapibus, tellus ac
+                                cursus tortor mauris
+                                condimentum fermentum
+                                massa justo sit amet purus
+                                sit amet fermentum.”
+                            </p>
+                            <h2>Cory Zamora</h2>
+                            <p>Marketing Specialist</p>
+                        </div>
                     </div>
-                    <div className="review__two-card">
-                        <p>
-                            “Fusce dapibus, tellus ac
-                            cursus tortor mauris
-                            condimentum fermentum
-                            massa justo sit amet purus
-                            sit amet fermentum.”
-                        </p>
-                        <h2>Cory Zamora</h2>
-                        <p>Marketing Specialist</p>
+                    <div className="review__two-d">
+                        <div className="review__two-card">
+                            <p>
+                                “Vivamus sagittis lacus vel
+                                augue laoreet rutrum
+                                faucibus dolor auctor.
+                                Vestibulum id ligula porta.
+                                Cras mattis consectetur.”
+                            </p>
+                            <h2>Coriss Ambady</h2>
+                            <p>Financial Analyst</p>
+                        </div>
+                        <div className="review__two-card">
+                            <p>
+                                “Fusce dapibus, tellus ac
+                                cursus tortor mauris
+                                condimentum fermentum
+                                massa justo sit amet purus
+                                sit amet fermentum.”
+                            </p>
+                            <h2>Cory Zamora</h2>
+                            <p>Marketing Specialist</p>
+                        </div>
                     </div>
-                    <div className="review__two-card">
-                        <p>
-                            “Curabitur blandit tempus
-                            porttitor. Vivamus sagittis
-                            lacus vel augue laoreet
-                            rutrum faucibus dolor eu
-                            rutrum. Nulla vitae libero.”
-                        </p>
-                        <h2>Nikolas Brooten</h2>
-                        <p>Sales Manager</p>
-                    </div>
-                    <div className="review__two-card">
-                        <p>
-                            “Etiam adipiscing tincidunt
-                            elit convallis felis suscipit ut.
-                            Phasellus rhoncus eu
-                            tincidunt auctor nullam
-                            rutrum, pharetra augue.”
-                        </p>
-                        <h2>Coriss Ambady</h2>
-                        <p>Financial Analyst</p>
-                    </div>
+                    
                 </div>
             </div>
         </section>
