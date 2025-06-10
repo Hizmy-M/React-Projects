151173000e2c4c5e5d0f7b90141b7f4fa422030b
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-10 17:55:57 +0530

    feat: file created -> semantic done

---
+++ 05.terraConnect-web/src/HeroSection.jsx
@@ -0,0 +1,16 @@
+export default function HeroSection() {
+    return (
+        <main>
+            <div className="section__container">
+                <div className="section__one">
+                    <h1>Network <span>solutions</span> for worldwide communication</h1>
+                    <h2>We are a company that focuses on establishing long-term relationship with customers</h2>
+                    <button className="primary-button">Explore Now</button>
+                </div>
+                <div className="section__two">
+                    <img src="/src/assets/image-01.png" alt="hero-image" />
+                </div>
+            </div>
+        </main>
+    )
+}
