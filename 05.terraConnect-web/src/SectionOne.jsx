122beafcea7990522abfb60871f281c159368e20
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-10 19:02:22 +0530

    deat: sectionOne -> semantic done

--- 05.terraConnect-web/src/SectionOne.jsx
+++ 05.terraConnect-web/src/SectionOne.jsx
@@ -1,7 +1,31 @@
 export default function SectionOne() {
     return (
         <section>
-            
+            <div className="section__container">
+                <div className="section__one">
+                    <p>Have perfect control</p>
+                    <h2>We bring solutions to make life easier for our customers.</h2>
+                    <p> 
+                        Cum sociis natoque penatibus et magnis dis parturient montes,
+                        nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in, egestas
+                        eget quam. Praesent commodo cursus magna, vel scelerisque nisl
+                        consectetur et. Vivamus sagittis lacus vel augue rutrum.
+                    </p>
+                    <div>
+                        <ul>
+                            <li>Aenean quam ornare.Curabitur blandit.</li>
+                            <li>Nullam quis risus eget urna mollis ornare.</li>
+                        </ul>
+                        <ul>
+                            <li>Aenean quam ornare.Curabitur blandit.</li>
+                            <li>Nullam quis risus eget urna mollis ornare.</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="section__two">
+                    <img src="/src/assets/image-02.png" alt="section-one" />
+                </div>
+            </div>
         </section>
     )
 }
