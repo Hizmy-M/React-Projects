d85d734e638b8df82b88b8e1f5520d72dfc63f15
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-10 18:54:14 +0530

    added feature section

--- 05.terraConnect-web/src/App.jsx
+++ 05.terraConnect-web/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header.jsx"
 import HeroSection from "./HeroSection.jsx"
+import FeatureSection from "./FeatureSection.jsx"
 
 function App() {
     return(
         <>
             <Header />
             <HeroSection />
+            <FeatureSection />
         </>
     )
 }
