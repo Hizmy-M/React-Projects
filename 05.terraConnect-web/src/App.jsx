fa3b098180337e11f1e2df0d7675d7c1f7972256
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-10 20:44:48 +0530

    feat: added case study section

--- 05.terraConnect-web/src/App.jsx
+++ 05.terraConnect-web/src/App.jsx
@@ -4,6 +4,7 @@ import FeatureSection from "./FeatureSection.jsx"
 import SectionOne from "./SectionOne.jsx"
 import SectionTwo from "./SectionTwo.jsx"
 import AboutUs from  "./AboutUs.jsx"
+import CaseStudies from "./CaseStudies.jsx"
 
 function App() {
     return(
@@ -14,6 +15,7 @@ function App() {
             <SectionOne />
             <SectionTwo />
             <AboutUs />
+            <CaseStudies />
         </>
     )
 }
