617c7005b55a1aac2ae41e0d6bb4e7d7c0a46870
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-11 14:51:46 +0530

    typography fixed -> communhity section

--- 05.terraConnect-web/src/CommunitySection.jsx
+++ 05.terraConnect-web/src/CommunitySection.jsx
@@ -1,9 +1,9 @@
 export default function CommunitySection() {
     return (
         <section>
-            <div className="community__section">
-                <span>Join Our Community</span>
-                <h1>We are trusted by over 5000+ clients. Join them now and grow your business.</h1>
+            <div className="community__section width">
+                <span className="logo-color highlite-title">Join our community</span>
+                <h1>We are trusted by over 5000+ clients. <br /> Join them now and grow your business.</h1>
                 <button className="primary-button">Get Started</button>
             </div>
         </section>
