21534c7c5be270c10476911b1a3e0f8129caa450
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-11 15:19:09 +0530

    community section completed

--- 05.terraConnect-web/src/CommunitySection.jsx
+++ 05.terraConnect-web/src/CommunitySection.jsx
@@ -1,7 +1,7 @@
 export default function CommunitySection() {
     return (
         <section>
-            <div className="community__section width">
+            <div className="community__section backgorund extra-space">
                 <span className="logo-color highlite-title">Join our community</span>
                 <h1>We are trusted by over 5000+ clients. <br /> Join them now and grow your business.</h1>
                 <button className="primary-button">Get Started</button>
