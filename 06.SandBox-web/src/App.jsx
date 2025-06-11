0dfddd46c3ae5889944928aeba91d2a5357418c1
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-11 20:17:43 +0530

    Added section one and two to the list

--- 06.SandBox-web/src/App.jsx
+++ 06.SandBox-web/src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./Header"
+import SectionOne from "./SectionOne";
+import SectionTwo from "./SectionTwo";
 
 function App() {
     return (
         <>
             <Header />
+            <SectionOne />
+            <SectionTwo />
         </>
     )
 }
