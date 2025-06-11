9a72389ae859f7ea43b2ce3e4a38a87ddf02a497
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-11 13:19:59 +0530

    feat: case study section done stlyes

--- 05.terraConnect-web/src/CaseStudies.jsx
+++ 05.terraConnect-web/src/CaseStudies.jsx
@@ -1,22 +1,22 @@
 export default function CaseStudies() {
     return (
-        <section>
-            <div className="case-studies__container">
+        <section className="extra-space">
+            <div className="case-studies__container width">
                 <div className="case-studies__one">
-                    <span>Case Studies</span>
-                    <h2>Check out some of our awesome projects with creative ideas and great design.</h2>
+                    <span className="logo-color highlite-title">Case Studies</span>
+                    <h2>Check out some of our awesome projects <br /> with creative ideas and great design.</h2>
                 </div>
                 <div className="case-studies__two">
                     <div className="case-studies__two-card">
-                        <img src="/src/assets/image-04.png" alt="image-04" />
+                        <img className="image-responsive" src="/src/assets/image-04.png" alt="image-04" />
                         <p>Ligula tristique quis risus</p>
                     </div>
                     <div className="case-studies__two-card">
-                        <img src="/src/assets/image-05.png" alt="image-04" />
+                        <img  className="image-responsive" src="/src/assets/image-05.png" alt="image-04" />
                         <p>Nullam id dolor elit id nibh</p>
                     </div>
                     <div className="case-studies__two-card">
-                        <img src="/src/assets/image-06.png" alt="image-04" />
+                        <img  className="image-responsive" src="/src/assets/image-06.png" alt="image-04" />
                         <p>Ultricies fusce porta elit</p>
                     </div>
                 </div>
