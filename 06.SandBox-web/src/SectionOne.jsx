491dab28a10f879a0e6f4c11fdfa3815d85cef90
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-11 20:17:18 +0530

    feat: file created -> semantic done

---
+++ 06.SandBox-web/src/SectionOne.jsx
@@ -0,0 +1,13 @@
+export default function SectionOne() {
+    return (
+        <main>
+            <div className="section__container">
+                <h1>We bring solutions to make life easier</h1>
+                <p>We are a creative company that focuses on long term relationships with customers.</p>
+                <button>Read More</button>
+            </div>
+        </main>
+    )
+}
+
+
