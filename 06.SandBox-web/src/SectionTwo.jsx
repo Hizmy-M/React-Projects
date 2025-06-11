6fc82b8b154f91905f4b8facde12e934ed0ed695
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-11 20:17:29 +0530

    feat: file created -> semantic done

---
+++ 06.SandBox-web/src/SectionTwo.jsx
@@ -0,0 +1,11 @@
+export default function SectionTwo() {
+    return (
+        <section>
+            <div className="section__container">
+                <h1>We bring solutions to make life easier</h1>
+                <p>We are a creative company that focuses on long term relationships with customers.</p>
+                <button>Read More</button>
+            </div>
+        </section>
+    )
+}
