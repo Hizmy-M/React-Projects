0787940d85f13522eca698803861af4e87ae1193
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-12 01:27:35 +0530

    feat: file created

---
+++ 06.SandBox-web/src/CollectIdeas.jsx
@@ -0,0 +1,12 @@
+export default function CollectIdeas() {
+    <section>
+        <div className="collect__container">
+            <div className="collect__one">
+                
+            </div>
+            <div className="collect__two">
+
+            </div>
+        </div>
+    </section>
+}
