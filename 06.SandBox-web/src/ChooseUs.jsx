5b3745e3cecb18eb777f2ac91e4309b126818a72
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-11 22:02:49 +0530

    Feat: File created -> chooseus section semantic done

---
+++ 06.SandBox-web/src/ChooseUs.jsx
@@ -0,0 +1,41 @@
+export default function ChooseUs() {
+    return (
+        <section>
+            <div className="choose-us__container">
+                <div className="choose-us__one">
+                    <span>Why choose sandbox?</span>
+                    <h2>Here are a few reasons why our <br /> customers choose Sandbox</h2>
+                </div>
+                <div className="choose-us__two">
+                    <div className="choose-us__card">
+                        <div className="choose-us__card-image">
+                            <img src="/src/assets/icon-05.png" alt="icon-05" />
+                        </div>
+                        <div className="choose-us__card-content">
+                            <h3>Collect Ideas</h3>
+                            <p>Duis mollis commodo luctus cursus commodo tortor mauris.</p>
+                        </div>
+                    </div>
+                    <div className="choose-us__card">
+                        <div className="choose-us__card-image">
+                            <img src="/src/assets/icon-06.png" alt="icon-05" />
+                        </div>
+                        <div className="choose-us__card-content">
+                            <h3>Data Analysis</h3>
+                            <p>Duis mollis commodo luctus cursus commodo tortor mauris.</p>
+                        </div>
+                    </div>
+                    <div className="choose-us__card">
+                        <div className="choose-us__card-image">
+                            <img src="/src/assets/icon-07.png" alt="icon-05" />
+                        </div>
+                        <div className="choose-us__card-content">
+                            <h3>Finalize Product</h3>
+                            <p>Duis mollis commodo luctus cursus commodo tortor mauris.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
