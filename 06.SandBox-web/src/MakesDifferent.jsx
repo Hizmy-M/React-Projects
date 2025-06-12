690a08a2c10de0f3d16e5a3b20a87d543e896eb2
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-12 12:48:50 +0530

    feat: file created -> semantic done

---
+++ 06.SandBox-web/src/MakesDifferent.jsx
@@ -0,0 +1,61 @@
+export default function MakesDifferent() {
+    return (
+        <section>
+            <div className="different__container">
+                <div className="different__one">
+                    <div className="different__image-one">
+                        <img src="/src/assets/image-06.png" alt="image-06" />
+                    </div>
+                    <div className="different__image-two">
+                        <img src="/src/assets/image-07.png" alt="image-07" />
+                        <img src="/src/assets/image-08.png" alt="image-08" />
+                    </div>
+                </div>
+                <div className="different__two">
+                    <div className="different__content-one">
+                        <span>What makes you different?</span>
+                        <h2>We bring solutions to make life easier for our customers</h2>
+                    </div>
+                    <div className="different__content-two">
+                        <div className="different-card">
+                            <div>
+                                <img src="/src/assets/icon-09.png" alt="icon-09" />
+                            </div>
+                            <div>
+                                <p>Creativity</p>
+                                <span>Curabitur blandit lacus portitor ridiculus mus.</span>
+                            </div>
+                        </div>
+                        <div className="different-card">
+                            <div>
+                                <img src="/src/assets/icon-10.png" alt="icon-10" />
+                            </div>
+                            <div>
+                                <p>Innovative Thinking</p>
+                                <span>Curabitur blandit lacus portitor ridiculus mus.</span>
+                            </div>
+                        </div>
+                        <div className="different-card">
+                            <div>
+                                <img src="/src/assets/icon-11.png" alt="icon-11" />
+                            </div>
+                            <div>
+                                <p>Rapid Solutions</p>
+                                <span>Curabitur blandit lacus portitor ridiculus mus.</span>
+                            </div>
+                        </div>
+                        <div className="different-card">
+                            <div>
+                                <img src="/src/assets/icon-12.png" alt="icon-12" />
+                            </div>
+                            <div>
+                                <p>Top-Notch Support</p>
+                                <span>Curabitur blandit lacus portitor ridiculus mus.</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
