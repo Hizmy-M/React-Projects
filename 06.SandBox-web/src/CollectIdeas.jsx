436b0b3941446eeae2b4b3de28ceeb23a4587b5e
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-12 12:03:55 +0530

    feat: file created -> semantic done

--- 06.SandBox-web/src/CollectIdeas.jsx
+++ 06.SandBox-web/src/CollectIdeas.jsx
@@ -2,10 +2,22 @@ export default function CollectIdeas() {
     <section>
         <div className="collect__container">
             <div className="collect__one">
-                
+                <img src="/src/assets/image-01.png" alt="image-01" />
             </div>
             <div className="collect__two">
-
+                <h2>Collect Ideas</h2>
+                <p> 
+                    Etiam porta sem malesuada magna mollis euismod. Donec ullamcorper nulla non
+                    metus auctor fringilla. Morbi leo risus, porta ac consectetur ac, vestibulum at
+                    eros. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh,
+                    ut fermentum massa justo sit amet risus. Nullam quis risus eget urna.
+                </p>
+                <ul>
+                    <li>Aenean eu leo quam. Pellentesque ornare.</li>
+                    <li>Nullam quis risus eget urna mollis ornare.</li>
+                    <li>Donec id elit non mi porta gravida at eget.</li>
+                </ul>
+                <button>Learn More</button>
             </div>
         </div>
     </section>
