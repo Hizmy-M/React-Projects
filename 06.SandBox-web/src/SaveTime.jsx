437d5f6df71d4b14fe5cd51d098722c88b46a701
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-12 12:08:49 +0530

    feat: file created -> save time section semantic done

---
+++ 06.SandBox-web/src/SaveTime.jsx
@@ -0,0 +1,24 @@
+export default function SaveTime() {
+    return (
+        <section>
+            <div className="save__container">
+                <div className="save__one">
+                    <p>Save Time and Money</p>
+                    <h2>Save your time and money by <br /> choosing our professional team</h2>
+                </div>
+                <div className="save__two"> 
+                    <div className="save__two-icon">
+                        <img src="/src/assets/icon-07.png" alt="icon-07" />
+                        <span>95%</span>
+                        <p>Customer Satisfaction</p>
+                    </div>
+                    <div className="save__two-icon">
+                        <img src="/src/assets/icon-08.png" alt="icon-08" />
+                        <span>80%</span>
+                        <p>Increase Sales</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
