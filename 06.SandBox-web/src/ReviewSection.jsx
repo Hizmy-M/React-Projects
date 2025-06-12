1e0f1d1fbb6558262f94a0c8ffd139fdcdff27b0
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-12 12:12:28 +0530

    feat: file created -> semantic done

---
+++ 06.SandBox-web/src/ReviewSection.jsx
@@ -0,0 +1,20 @@
+export default function ReviewSection() {
+    return (
+        <section>
+            <div className="review__container">
+                <div className="review__one">
+                    <img src="/src/assets/image-02.png" alt="" />
+                </div>
+                <div className="review__two">
+                    <p>
+                        “Vivamus sagittis lacus vel augue laoreet rutrum
+                        faucibus dolor auctor. Vestibulum ligula porta felis
+                        euismod semper. Cras justo odio.”
+                    </p>
+                    <h2>Corris Ambady</h2>
+                    <span>Financial Analyst</span>
+                </div>
+            </div>
+        </section>
+    )
+}
