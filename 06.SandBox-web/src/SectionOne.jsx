3cb7da38f2ab87b37c1bbb96eaf651dbfa8c8c0f
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-14 22:55:12 +0530

    feat:  hero section style started

--- 06.SandBox-web/src/SectionOne.jsx
+++ 06.SandBox-web/src/SectionOne.jsx
@@ -1,8 +1,8 @@
 export default function SectionOne() {
     return (
-        <main>
-            <div className="section__container">
-                <h1>We bring solutions to make life easier</h1>
+        <main className="background-blue">
+            <div className="section__container width">
+                <h1>We bring solutions to <br /> make life easier</h1>
                 <p>We are a creative company that focuses on long term relationships with customers.</p>
                 <button>Read More</button>
             </div>
