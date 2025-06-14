2b1cfc5be4ced36f3d357894c9b61b074e182de4
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-15 02:50:47 +0530

    section one done

--- 06.SandBox-web/src/SectionOne.jsx
+++ 06.SandBox-web/src/SectionOne.jsx
@@ -3,8 +3,8 @@ export default function SectionOne() {
         <main className="background-blue">
             <div className="section__container width">
                 <h1>We bring solutions to <br /> make life easier</h1>
-                <p>We are a creative company that focuses on <br /> long term relationships with customers.</p>
-                <button>Read More</button>
+                <p>We are a creative company that focuses on long <br /> term relationships with customers.</p>
+                <button className="primary-btn" >Read More</button>
             </div>
         </main>
     )
