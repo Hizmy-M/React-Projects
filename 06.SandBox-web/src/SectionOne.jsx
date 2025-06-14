c2abcd182788a10f00afe705329987e7397dd233
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-15 02:33:02 +0530

    added line break

--- 06.SandBox-web/src/SectionOne.jsx
+++ 06.SandBox-web/src/SectionOne.jsx
@@ -3,7 +3,7 @@ export default function SectionOne() {
         <main className="background-blue">
             <div className="section__container width">
                 <h1>We bring solutions to <br /> make life easier</h1>
-                <p>We are a creative company that focuses on long term relationships with customers.</p>
+                <p>We are a creative company that focuses on <br /> long term relationships with customers.</p>
                 <button>Read More</button>
             </div>
         </main>
