5ded41c94f634c8b42dd0620c595401f88d5ea42
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-15 02:53:20 +0530

    header slignemnt fixed

--- 06.SandBox-web/src/SectionTwo.jsx
+++ 06.SandBox-web/src/SectionTwo.jsx
@@ -1,11 +1,13 @@
-export default function SectionTwo() {
+export default function SectionOne() {
     return (
-        <section>
-            <div className="section__container">
-                <h1>We bring solutions to make life easier</h1>
-                <p>We are a creative company that focuses on long term relationships with customers.</p>
-                <button>Read More</button>
+        <section className="background-blue">
+            <div className="section__container width">
+                <h1>We bring solutions to <br /> make life easier</h1>
+                <p>We are a creative company that focuses on long <br /> term relationships with customers.</p>
+                <button className="primary-btn" >Read More</button>
             </div>
         </section>
     )
 }
+
+
