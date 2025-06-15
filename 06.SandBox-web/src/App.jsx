0757f9ca89137f292b90ef00b16591838510001c
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-15 23:57:51 +0530

    fixed footer

--- 06.SandBox-web/src/App.jsx
+++ 06.SandBox-web/src/App.jsx
@@ -1,15 +1,29 @@
 import Header from "./Header"
-import CardSection from "./CardSection"
+import SectionOne from "./SectionOne";
+import CardSection from "./CardSection";
 import ChooseUs from "./ChooseUs"
-import CollectIdeas from "./CollectIdeas"
+import CollectIdeas from "./CollectIdeas";
+import SaveTime from "./SaveTime";
+import ReviewSection from "./ReviewSection";
+import LatestProjects from "./LatestProjects";
+import MakesDifferent from "./MakesDifferent";
+import SectionTwo from "./SectionTwo";
+import Footer from "./Footer";
 
 function App() {
     return (
         <>
             <Header />
+            <SectionOne />
             <CardSection />
             <ChooseUs />
             <CollectIdeas />
+            <SaveTime />
+            <ReviewSection />
+            <LatestProjects />
+            <MakesDifferent />
+            <SectionTwo />
+            <Footer />
         </>
     )
 }
