02a2cd972b53201c7b8c0cbfb4018cb9d69c81c6
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-15 22:40:36 +0530

    collect ideas style done

--- 06.SandBox-web/src/CollectIdeas.jsx
+++ 06.SandBox-web/src/CollectIdeas.jsx
@@ -1,9 +1,9 @@
 export default function CollectIdeas() {
     return(
         <section>
-            <div className="collect__container">
+            <div className="collect__container width">
                 <div className="collect__one">
-                    <img src="/src/assets/image-01.png" alt="image-01" />
+                    <img className="main-image" src="/src/assets/image-01.png" alt="image-01" />
                 </div>
                 <div className="collect__two">
                     <h2>Collect Ideas</h2>
@@ -18,7 +18,7 @@ export default function CollectIdeas() {
                         <li>Nullam quis risus eget urna mollis ornare.</li>
                         <li>Donec id elit non mi porta gravida at eget.</li>
                     </ul>
-                    <button>Learn More</button>
+                    <button className="primary-btn secondary-btn">Learn More</button>
                 </div>
             </div>
         </section>
