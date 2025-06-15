ba00c6037b9a86d8213e5f50546cc8bc5dcf5563
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-15 22:21:22 +0530

    fixed -> cmoponenets was hidden fixed issue -> return was missing in compoenet

--- 06.SandBox-web/src/App.jsx
+++ 06.SandBox-web/src/App.jsx
@@ -1,29 +1,15 @@
 import Header from "./Header"
-import SectionOne from "./SectionOne";
-import CardSection from "./CardSection";
+import CardSection from "./CardSection"
 import ChooseUs from "./ChooseUs"
-import CollectIdeas from "./CollectIdeas";
-import SaveTime from "./SaveTime";
-import ReviewSection from "./ReviewSection";
-import LatestProjects from "./LatestProjects";
-import MakesDifferent from "./MakesDifferent";
-import SectionTwo from "./SectionTwo";
-import Footer from "./Footer";
+import CollectIdeas from "./CollectIdeas"
 
 function App() {
     return (
         <>
             <Header />
-            <SectionOne />
             <CardSection />
             <ChooseUs />
             <CollectIdeas />
-            <SaveTime />
-            <ReviewSection />
-            <LatestProjects />
-            <MakesDifferent />
-            <SectionTwo />
-            <Footer />
         </>
     )
 }
