6f90bfa9c34cb25cebfa9c47015ca8d7673de9ed
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-16 01:09:40 +0530

    save time section icon styles completed

--- 06.SandBox-web/src/SaveTime.jsx
+++ 06.SandBox-web/src/SaveTime.jsx
@@ -1,19 +1,19 @@
 export default function SaveTime() {
     return (
         <section>
-            <div className="save__container">
+            <div className="save__container width">
                 <div className="save__one">
-                    <p>Save Time and Money</p>
-                    <h2>Save your time and money by <br /> choosing our professional team</h2>
+                    <p className="sub-font">Save Time and Money</p>
+                    <h2 className="main-font">Save your time and money by <br /> choosing our professional team</h2>
                 </div>
                 <div className="save__two"> 
                     <div className="save__two-icon">
-                        <img src="/src/assets/icon-07.png" alt="icon-07" />
+                        <img className="savetime-icon" src="/src/assets/icon-07.png" alt="icon-07" />
                         <span>95%</span>
                         <p>Customer Satisfaction</p>
                     </div>
                     <div className="save__two-icon">
-                        <img src="/src/assets/icon-08.png" alt="icon-08" />
+                        <img className="savetime-icon" src="/src/assets/icon-08.png" alt="icon-08" />
                         <span>80%</span>
                         <p>Increase Sales</p>
                     </div>
