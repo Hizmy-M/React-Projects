782ec902b38825bfe7bcaf2b02d2a828489ab958
Hizmy-M <hizmy.mohammed@gmail.com>
2025-06-15 20:26:52 +0530

    Crad section -> completed

--- 06.SandBox-web/src/CardSection.jsx
+++ 06.SandBox-web/src/CardSection.jsx
@@ -1,57 +1,61 @@
 export default function CardSection() {
     return (
         <section>
-            <div className="card__container">
-                <div className="card">
-                    <div className="card__icon">
-                        <img src="/src/assets/icon-01.png" alt="card-icon" />
+            <div className="card__container width">
+                <div className="card-two">
+                    <div className="card">
+                        <div className="card__icon">
+                            <img className="icon-image" src="/src/assets/icon-01.png" alt="card-icon" />
+                        </div>
+                        <div className="card__contents">
+                            <h1>Content Marketing</h1>
+                            <p> 
+                                Nulla vitae elit libero, a pharetra augue. Donec id elit non mi 
+                                porta gravida at egetmetus cras justo.
+                            </p>
+                            <span>Learn More</span>
+                        </div>
                     </div>
-                    <div className="card__contents">
-                        <h1>Content Marketing</h1>
-                        <p> 
-                            Nulla vitae elit libero, a pharetra augue. Donec id elit non mi 
-                            porta gravida at egetmetus cras justo.
-                        </p>
-                        <span>Learn More</span>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="card__icon">
-                        <img src="/src/assets/icon-02.png" alt="card-icon" />
-                    </div>
-                    <div className="card__contents">
-                        <h1>Social Engagement</h1>
-                        <p> 
-                            Nulla vitae elit libero, a pharetra augue. Donec id elit non mi 
-                            porta gravida at egetmetus cras justo.
-                        </p>
-                        <span>Learn More</span>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="card__icon">
-                        <img src="/src/assets/icon-03.png" alt="card-icon" />
-                    </div>
-                    <div className="card__contents">
-                        <h1>Identity & Branding</h1>
-                        <p> 
-                            Nulla vitae elit libero, a pharetra augue. Donec id elit non mi 
-                            porta gravida at egetmetus cras justo.
-                        </p>
-                        <span>Learn More</span>
+                    <div className="card">
+                        <div className="card__icon">
+                            <img className="icon-image" src="/src/assets/icon-02.png" alt="card-icon" />
+                        </div>
+                        <div className="card__contents">
+                            <h1>Social Engagement</h1>
+                            <p> 
+                                Nulla vitae elit libero, a pharetra augue. Donec id elit non mi 
+                                porta gravida at egetmetus cras justo.
+                            </p>
+                            <span>Learn More</span>
+                        </div>
                     </div>
                 </div>
-                <div className="card">
-                    <div className="card__icon">
-                        <img src="/src/assets/icon-04.png" alt="card-icon" />
+                <div className="card-two">
+                    <div className="card">
+                        <div className="card__icon">
+                            <img className="icon-image" src="/src/assets/icon-03.png" alt="card-icon" />
+                        </div>
+                        <div className="card__contents">
+                            <h1>Identity & Branding</h1>
+                            <p> 
+                                Nulla vitae elit libero, a pharetra augue. Donec id elit non mi 
+                                porta gravida at egetmetus cras justo.
+                            </p>
+                            <span>Learn More</span>
+                        </div>
                     </div>
-                    <div className="card__contents">
-                        <h1>Product Design</h1>
-                        <p> 
-                            Nulla vitae elit libero, a pharetra augue. Donec id elit non mi 
-                            porta gravida at egetmetus cras justo.
-                        </p>
-                        <span>Learn More</span>
+                    <div className="card">
+                        <div className="card__icon">
+                            <img className="icon-image" src="/src/assets/icon-03.png" alt="card-icon" />
+                        </div>
+                        <div className="card__contents">
+                            <h1>Product Design</h1>
+                            <p> 
+                                Nulla vitae elit libero, a pharetra augue. Donec id elit non mi 
+                                porta gravida at egetmetus cras justo.
+                            </p>
+                            <span>Learn More</span>
+                        </div>
                     </div>
                 </div>
             </div>
